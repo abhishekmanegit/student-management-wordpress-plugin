60c54a1cf220ead998e3a29a0e171ef8d27f3256
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-01-16 19:05:34 +0530

    Intial Plugin Setup

---
+++ student-management.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Student Management Plugin
+ * Description: Custom WordPress plugin to manage student records.
+ * Version: 0.1
+ * Author: Abhishek Mane
+ */
+
+defined('ABSPATH') || exit;
