677ff3bf531918fa6c41a886d48cc4cd96db11e9
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-01-18 23:29:16 +0530

    Add admin menu for student management

--- student-management.php
+++ student-management.php
@@ -19,3 +19,24 @@ function smp_register_student_cpt() {
 
     register_post_type('student', $args);
 }
+
+add_action('admin_menu', 'smp_add_admin_menu');
+
+function smp_add_admin_menu() {
+    add_menu_page(
+        'Student Management',     // Page title
+        'Students',               // Menu title
+        'manage_options',         // Capability
+        'student-management',     // Menu slug
+        'smp_admin_page',         // Callback function
+        'dashicons-welcome-learn-more', // Icon
+        6                          // Position
+    );
+}
+
+function smp_admin_page() {
+    echo '<div class="wrap">';
+    echo '<h1>Student Management</h1>';
+    echo '<p>Welcome to the Student Management Plugin.</p>';
+    echo '</div>';
+}
