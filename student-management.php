0d64bacc7c628854eb3419e1be1e435f4bf2a162
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-01-19 17:38:53 +0530

    Add student meta fields (roll number, class)

--- student-management.php
+++ student-management.php
@@ -40,3 +40,30 @@ function smp_admin_page() {
     echo '<p>Welcome to the Student Management Plugin.</p>';
     echo '</div>';
 }
+
+add_action('add_meta_boxes', 'smp_add_student_meta_box');
+
+function smp_add_student_meta_box() {
+    add_meta_box(
+        'smp_student_details',
+        'Student Details',
+        'smp_student_meta_box_callback',
+        'student',
+        'normal',
+        'default'
+    );
+}
+
+function smp_student_meta_box_callback($post) {
+    $roll = get_post_meta($post->ID, '_smp_roll', true);
+    $class = get_post_meta($post->ID, '_smp_class', true);
+    ?>
+
+    <label for="smp_roll">Roll Number</label><br>
+    <input type="text" id="smp_roll" name="smp_roll" value="<?php echo esc_attr($roll); ?>" /><br><br>
+
+    <label for="smp_class">Class</label><br>
+    <input type="text" id="smp_class" name="smp_class" value="<?php echo esc_attr($class); ?>" />
+
+    <?php
+}
