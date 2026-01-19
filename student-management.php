eff3b82bbdba0860ad1df9e3ed4ba63e093d6edc
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-01-19 18:51:11 +0530

    Add REST API endpoint to fetch students

--- student-management.php
+++ student-management.php
@@ -67,3 +67,58 @@ function smp_student_meta_box_callback($post) {
 
     <?php
 }
+
+add_action('save_post', 'smp_save_student_meta');
+
+function smp_save_student_meta($post_id) {
+
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        return;
+    }
+
+    if (isset($_POST['smp_roll'])) {
+        update_post_meta($post_id, '_smp_roll', sanitize_text_field($_POST['smp_roll']));
+    }
+
+    if (isset($_POST['smp_class'])) {
+        update_post_meta($post_id, '_smp_class', sanitize_text_field($_POST['smp_class']));
+    }
+}
+
+add_action('rest_api_init', 'smp_register_student_api');
+
+function smp_register_student_api() {
+    register_rest_route('smp/v1', '/students', array(
+        'methods'  => 'GET',
+        'callback' => 'smp_get_students',
+    ));
+}
+
+function smp_get_students() {
+
+    $args = array(
+        'post_type'      => 'student',
+        'posts_per_page' => -1,
+    );
+
+    $query = new WP_Query($args);
+
+    $students = array();
+
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+
+            $students[] = array(
+                'id'    => get_the_ID(),
+                'name'  => get_the_title(),
+                'roll'  => get_post_meta(get_the_ID(), '_smp_roll', true),
+                'class' => get_post_meta(get_the_ID(), '_smp_class', true),
+            );
+        }
+    }
+
+    wp_reset_postdata();
+
+    return $students;
+}
